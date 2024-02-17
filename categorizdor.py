e082205ad868c1d3ad7ff5c1be58c55fe39f811f
Lucas Moura <lsam.lucmoura0410@gmail.com>
2024-02-17 15:34:49 -0300

    versao atendente de mecanica

--- categorizdor.py
+++ categorizdor.py
@@ -1,32 +1,61 @@
 from openai import OpenAI
 from dotenv import load_dotenv
 import os
+#APENAS COLOQUE O CÓDIGO PARA FUNCIONAR
 
 load_dotenv()
 cliente = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+versao = "gpt-3.5-turbo"
+
+prompt_sistema = """
+    Você é um assistente de loja.
+    Sua função é ajudar os clientes falando sobre o produto.
+    
+    # Lista de categorias válidas:
+    - Ferramenta
+    - Sistema de Admissão
+    - Sistema de transmissão
+    - Parte Elétrica
+    - Freios
+    - Sistema de escape
+    - Fluidos
+    - Sistema de ignição
+    
+    
+    
+    # Formato de Saída
+    Olá, sou o mini_luc.
+    Posso ver que a peça que você pediu foi:
+    Produto: Nome do produto
+    Categoria: Apresente a categoria do produto
+    
+    # Exemplo de Saída
+    Olá, sou o mini_luc.
+    Posso ver que a peça que você pediu foi:
+    Produto: Disco de freio
+    Categoria: Freios
+    
+"""
+
+prompt_usuario = input("Olá!\nMe diga qual produto que você quer saber a categoria:\n")
 
 resposta = cliente.chat.completions.create(
     messages=[
         {
             "role": "system",
-            "content": """
-            Classifique o produto abaixo em uma das categorias:Higiene
-            Pessoal, Moda ou Casa e dê uma descrição da categoria
-            """
+            "content":prompt_sistema
         },
         {
             "role" : "user",
-            "content" : """""
-            Camiseta
-            """
+            "content" : prompt_usuario
         }
         
     ],
-    model="gpt-3.5-turbo",
-    temperature = 0.5,
+    model=versao,
+    temperature = 1,
     max_tokens=257,
-    n = 3,
+
 )
 
-for contador in range(0,3):
-    print(resposta.choices[0].message.content)
+print(resposta.choices[0].message.content)
+    
